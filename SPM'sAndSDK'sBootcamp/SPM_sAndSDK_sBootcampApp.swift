7cfa533ac781b4d4e813452410e988c342c9983d
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-10-17 16:53:25 +0300

    First Commit

---
+++ SPM'sAndSDK'sBootcamp/SPM_sAndSDK_sBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPM_sAndSDK_sBootcampApp.swift
+//  SPM'sAndSDK'sBootcamp
+//
+//  Created by Oğuzhan Abuhanoğlu on 17.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SPM_sAndSDK_sBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
